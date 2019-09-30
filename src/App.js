ff2b11efdb5d8c7462ecba9246a09aedc6ee3d7b
Lucas Arce <lucasarce@MacBook-Pro-de-Lucas.local>
2019-09-30 11:32:54 -0400

    Parte 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+//  Que pasa en el BrowserRouter? Hay dos formas de exportar
+// La primera por 'default'
+// export default Test;
+// import Test from './test';
+// La segunda
+// export {
+// Test,
+// Test1,
+// Test2,
+// Test3  
+//};
+// import {Test as pepe, Test2, Test3 } from './test';
+
 import logo from './logo.svg';
 import './App.css';
+import AxiosTest from './AxiosTest'; //ejemplo con high order component with styles 
+
+function Index() {
+  return <><h2>Home</h2><AxiosTest/></>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     // <div>
+     //   <div className="App"> style={{ backgroundColor: 'grey' }}>
+     // <header className="App-header">
+     //   <img src={logo} className="App-logo" alt="logo" />
+     //   </header>
+     //   </div>
+     //   <AxiosTest />
+     // </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+                               
+        <Route path="/" exact component={Index} /> 
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+
+      </div>
+    </Router>
   );
 }
 
